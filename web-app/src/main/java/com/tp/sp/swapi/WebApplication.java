17b6612f91618d438215054a0ec3a0ecc49abc7e
TomaszPazdziurek <tomasz.pazdziurek@nekken.pl>
2019-11-10 20:13:42 +0100

    Changed WAR file build to create executable WAR

--- web-app/src/main/java/com/tp/sp/swapi/WebApplication.java
+++ web-app/src/main/java/com/tp/sp/swapi/WebApplication.java
@@ -2,15 +2,9 @@ package com.tp.sp.swapi;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = "com.tp.sp.swapi")
-public class WebApplication extends SpringBootServletInitializer {
-
-  @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-    return builder.sources(WebApplication.class);
-  }
+public class WebApplication {
 
   public static void main(String[] args) {
     new SpringApplicationBuilder(WebApplication.class).run(args);
